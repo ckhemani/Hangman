a95110ca5eebc96264bf7dac19754ddc193484b3
Chandru <chandru.khemani@gmail.com>
2022-03-20 03:13:29 -0400

    Initial test

---
+++ src/main/java/Hangman.java
@@ -0,0 +1,8 @@
+public class Hangman {
+    public static void main(String[] args) {
+        welcome();
+    }
+    public static void welcome(){
+        System.out.println("\n lets Program Hangman\n");
+    }
+}
